6f4ea992571a6880daaaf424d015062c41f7c181
Muhammad Ghiyats Fatiha <muhammadg.fatiha@gmail.com>
2024-10-13 13:21:31 +0700

    Ubah code register

--- lib/register.dart
+++ lib/register.dart
@@ -1,91 +1,361 @@
+// ignore_for_file: camel_case_types, library_private_types_in_public_api, use_build_context_synchronously, dead_code
+
 import 'package:flutter/material.dart';
 
-class RegistrationPage extends StatefulWidget {
-  @override
-  _RegistrationPageState createState() => _RegistrationPageState();
+void main() {
+  runApp(const Landing());
 }
 
-class _RegistrationPageState extends State<RegistrationPage> {
-  final _formKey = GlobalKey<FormState>();
-
-  // Controllers for the form fields
-  TextEditingController _usernameController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+// Penamaan kelas harus PascalCase
+class Landing extends StatelessWidget {
+  const Landing({super.key});
 
   @override
-  void dispose() {
-    // Clean up the controllers when the widget is disposed
-    _usernameController.dispose();
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoadingPage(),
+    );
+  }
+}
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Menunggu 5 detik sebelum berpindah halaman
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          transitionDuration:
+              const Duration(milliseconds: 600), // Durasi transisi
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              const Register(), // Pastikan class Register sudah ada
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(1.0, 0.0); // Mulai dari kanan
+            const end = Offset.zero;
+            const curve = Curves.ease;
+
+            var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(
+              position: offsetAnimation,
+              child: child,
+            );
+          },
+        ),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-        centerTitle: true,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo2.png'),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+    );
+  }
+}
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  bool _obscureText1 = true; // Untuk Kata Sandi
+  bool _obscureText2 = true; // Untuk Konfirmasi Kata Sandi
+  List<bool> isSelected = [true, false, false, false]; // Untuk pilihan tim
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: <Widget>[
-              // Username Field
-              TextFormField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'Username'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a username';
-                  }
-                  return null;
-                },
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 70),
+              const Text(
+                'Daftar',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins',
+                  color: Colors.black,
+                ),
               ),
-              SizedBox(height: 16),
-
-              // Email Field
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
+              const SizedBox(height: 15),
+              Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 150,
+                  child: Image.asset(
+                      'assets/logo1.png'), // Ubah ke logo yang sesuai
+                ),
               ),
-              SizedBox(height: 16),
+              const SizedBox(height: 25),
 
-              // Password Field
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.length < 6) {
-                    return 'Password must be at least 6 characters long';
-                  }
-                  return null;
-                },
+              // Email Input Field
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Email',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 16,
+                  ),
+                ),
               ),
-              SizedBox(height: 16),
+              const SizedBox(height: 5),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'email anda',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.cyan, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.cyan, width: 2),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
 
-              // Register Button
+              // Password Input Field
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Kata Sandi',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                obscureText: _obscureText1,
+                decoration: InputDecoration(
+                  hintText: '',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.cyan, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.cyan, width: 2),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText1 ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText1 = !_obscureText1; // Toggle visibility
+                      });
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+
+              // Confirm Password Input Field
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Konfirmasi Kata Sandi',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                obscureText: _obscureText2,
+                decoration: InputDecoration(
+                  hintText: '',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.cyan, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.cyan, width: 2),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText2 ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText2 = !_obscureText2; // Toggle visibility
+                      });
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Pilihan Tim (Mikrobiologi, Dokter, PPI, PPRA)
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Daftar Sebagai Tim:',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 1),
+              const Padding(padding: EdgeInsets.all(5)),
+              ToggleButtons(
+                borderColor: Colors.grey,
+                fillColor: Colors.teal,
+                borderWidth: 2,
+                selectedBorderColor: Colors.teal,
+                selectedColor: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+                onPressed: (int index) {
+                  setState(() {
+                    for (int i = 0; i < isSelected.length; i++) {
+                      isSelected[i] = i == index;
+                    }
+                  });
+                },
+                isSelected: isSelected,
+                children: const <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 1),
+                    child: Text('Mikrobiologi'),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 1),
+                    child: Text('Dokter'),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 1),
+                    child: Text('PPI'),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 1),
+                    child: Text('PPRA'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+
+              // Tombol Daftar
               ElevatedButton(
                 onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Process data (for now just show a snackbar)
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Processing Data')),
-                    );
-                  }
+                  // Aksi saat tombol "Daftar" diklik
                 },
-                child: Text('Register'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.cyan,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 150,
+                    vertical: 20,
+                  ),
+                ),
+                child: const Text('Daftar',
+                    style: TextStyle(
+                      fontSize: 18,
+                    )),
+              ),
+              const SizedBox(height: 20),
+
+              // Sudah punya akun / Login dengan
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Sudah Punya Akun ?'),
+                  const SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () {
+                      // Aksi saat "Masuk" diklik
+                    },
+                    child: const Text(
+                      'Masuk',
+                      style: TextStyle(
+                        color: Colors.cyan,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              const Text('Atau lanjutkan dengan:'),
+              const SizedBox(height: 10),
+
+              // Tombol Sosial Media (Google, Facebook, Twitter)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Tombol Google
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/google.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    iconSize: 1,
+                    onPressed: () {},
+                  ),
+                  const SizedBox(width: 1),
+                  // Tombol Facebook
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/fb.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    iconSize: 1,
+                    onPressed: () {},
+                  ),
+                  const SizedBox(width: 1),
+                  // Tombol Twitter
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/x.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    iconSize: 1,
+                    onPressed: () {},
+                  ),
+                ],
               ),
             ],
           ),
